ba6f5aac2a27397a22776dc31b9d5ed37499214a
Project Scientist <slabeden@ucsc.edu>
2025-12-30 12:47:02 +0100

    Update budget_vert_simulator.py

--- budget_vert/budget_vert_simulator.py
+++ budget_vert/budget_vert_simulator.py
@@ -1,15 +1,21 @@
 import streamlit as st
+from pathlib import Path
 
 st.set_page_config(
-#    layout="wide",
     initial_sidebar_state="expanded",
 )
 
-st.logo("Logo_CdC_Aunis_sud.jpg")  
+# Chemin vers le logo
+BASE_DIR = Path(__file__).resolve().parent
+logo_path = BASE_DIR / "Logo_CdC_Aunis_sud.jpg"
+
+# Affichage du logo
+st.image(logo_path)
 
 with st.sidebar:
     st.caption("v1.0")
 
+# Navigation
 pg = st.navigation([
     st.Page("pages/axe1.py", title="🌏 Axe 1 - Atténuation Climat"),
     st.Page("pages/axe6.py", title="🦜 Axe 6 - Biodiversité"),
